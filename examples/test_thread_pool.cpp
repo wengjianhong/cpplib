19a238b1f761beac42263fdb8b32151f4c583803
wengjianhong <wengjianhong2099@163.com>
2023-05-21 11:18:18 +0800

    新增线程池：thread_pool.hpp

---
+++ examples/test_thread_pool.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include "../thread_pool.hpp"
+
+int fun(int taskid, int n)
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        sleep(1);
+    }
+    printf("task %d end\n", taskid);
+    return taskid * 100000 + n;
+}
+
+int main()
+{
+    CThreadPool pool(5);
+
+    std::future<int> ret[13];
+    for (int i = 0; i < 13; i++)
+    {
+        ret[i] = pool.enqueue(fun, i, 3);
+    }
+
+    printf("run...\n");
+    pool.run();
+    sleep(1);
+
+    printf("stop...\n");
+    pool.stop();
+    sleep(5);
+
+    printf("run...\n");
+    pool.run();
+
+    for (int i = 0; i < 13; i++)
+    {
+        printf("%d\n", ret[i].get());
+    }
+}
